c93fa928e5d0b446da4d069881da86fbef4aebdd
blissconnect <your@email.com>
2026-05-31 11:16:04 -0700

    Fix Candidate model import

--- routes/payment.js
+++ routes/payment.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Payment = require("../models/Payment");
-const Candidate = require("../models/Candidate");
+const Candidate = require("../models/candidate");
 const sendEmail = require("../email");
 
 // ======================
